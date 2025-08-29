cc7580ae5ee0ae920b73232b6388e7d6bef6325b
CoorayNTL <tl685998@gmail.com>
2025-08-29 18:06:24 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthEnabledWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
